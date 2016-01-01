193e783b1ab0d439f217a5e638b3ab63f887dfbd
zhanghw0354 <hiwin.chang@gmail.com>
2016-01-02 00:35:56 +0800

    update header and footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,8 @@
+<footer class="vfb-site-footer">
+  <div class="container">
+    <p>Powerd By Codeigniter & Bootstrap</p>
+  </div>
+</footer> 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="<?php echo base_url('public/js/jquery-1.11.3.min.js'); ?>"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
--- application/views/header.php
+++ application/views/header.php
@@ -9,6 +9,7 @@
 
     <!-- Bootstrap -->
     <link href="<?php echo base_url('public/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('public/css/footer.css'); ?>" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
